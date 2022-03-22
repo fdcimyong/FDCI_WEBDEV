fad85bd8b41ec71c2201658f4d62ec23f9043da9
fdcimyong <fdc.nuenos@gmail.com>
2022-03-22 21:35:04 +0800

    master: initial commit

---
+++ html_ex1/index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nickey's Hobbys</title>
+</head>
+<body>
+
+
+    <table border=1 width="600px">
+        <!-- this area is for the banner -->
+        <tr>
+            <td colspan="3">
+                <img width="600px" src="./20220226_061132.jpeg" alt="">
+            </td>
+        </tr>
+
+        <!-- this is for the items -->
+        <tr>
+            <td>
+                <div>
+                    <h4>Top Places</h4>
+                    <ol>
+                        <li>Apas</li>
+                        <li>Colon</li>
+                        <li>Pardo</li>
+                        <li>Minglanilla</li>
+                        <li>Carbon</li>
+                    </ol>
+                </div>
+            </td>
+            <td>
+                <div>
+                    <h4>Top Food</h4>
+                    <ol>
+                        <li>Lucky Me</li>
+                        <li>Extra Big</li>
+                        <li>Calamansi</li>
+                        <li>Sweet and Spicy</li>
+                        <li>Extra Hot</li>
+                    </ol>
+                </div>       
+            </td>
+            <td>
+                <div>
+                    <h4>Top Websites</h4>
+                    <ol>
+                        <li>
+                            <a target="_blank" href="https://www.youtube.com/">Youtube</a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="https://www.google.com/">Google</a>
+                            
+                        </li>
+                        <li>
+                            <a target="_blank" href="https://ph.yahoo.com">Yahoo</a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="https://facebook.com">Facebook</a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="https://nativecamp.net">Native Camp</a>
+                        </li>
+                    </ol>
+                </div>
+            </td>
+        </tr>
+
+        <!-- this is for the blog 1 -->
+        <tr>
+            <td colspan="3">
+                <ul>
+                    <li>
+                        <h4>Walay laag</h4>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis, quod magni placeat perspiciatis ratione nostrum nisi odit fuga, iusto eum libero minus ea est unde ducimus voluptatem rerum magnam! Adipisci!</p>
+                    </li>
+                </ul>
+            </td>
+        </tr>
+        <!-- this is for the footer -->
+        <tr>
+            <td colspan="3">Footer ko! Dili magbuot!</td>
+        </tr>
+    </table>
+
+</body>
+</html>
